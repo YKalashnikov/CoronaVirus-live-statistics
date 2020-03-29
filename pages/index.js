ec0a45fd5674ffbe171cb5c024364ee33404caf6
YKalashnikov <uk2709@gmail.com>
2020-03-29 09:12:07 -0700

    new features

--- pages/index.js
+++ pages/index.js
@@ -8,20 +8,34 @@ export default function IndexPage() {
   <link rel="icon" type="image/x-icon" href="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.vicksburgnews.com%2Fwp-content%2Fuploads%2F2020%2F02%2FCoronavirus-2019-nCoV-CDC-23312_without_background.png&f=1&nofb=1"/>
   <title>Corona Virus</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
   <meta property="image" content="https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/2019-nCoV-CDC-23312_without_background.png/1280px-2019-nCoV-CDC-23312_without_background.png"/>
+  
   <meta name="corona virus statistics" content="Corona Virus Live Statistics In The World" />
-  <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-NTCHHHP');`}} />
+  
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-162033101-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments)}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-162033101-1');
+</script>
+
+
+
 </Head>
 <noscript dangerouslySetInnerHTML={{__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NTCHHHP" height="0" width="0" style="display:none;visibility:hidden;"></iframe>`}} />
       <h2 style={{textAlign:'center'}}>WORLD CORONA VIRUS <a style={{cursor:'pointer', color:'blue', textDecorationLine:'underline', textDecorationColor:'lightblue'}}href="https://covid19.mathdro.id/api/og" target='_blank'>STATISTICS</a></h2>
+
         <Stats url ="https://covid19.mathdro.id/api"></Stats>
+
         <Country url='https://covid19.mathdro.id/api/countries'></Country>
         <div style={{display:'flex', flexDirection:'row', justifyContent:'center', alignItems:'center'}} > 
+
        <a style={{cursor:'pointer'}} href="mailto:uk2709@gmail.com" target='_blank'><img style={{height:'44px'}} src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmail.co.uk%2Fimg%2Ficons%2Ficon-mail.png&f=1&nofb=1"/></a>
+
        <a style={{cursor:'pointer'}} href="https://www.facebook.com/iurii.kalashnikov" target='_blank'><img style={{height:'34px'}} src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimg.icons8.com%2Fdotty%2F2x%2Ffacebook.png&f=1&nofb=1"/></a>
        </div>
         </div>)
