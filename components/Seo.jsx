2ffd8bf62500a0dca418fa2023d761e9caa6d312
YKalashnikov <uk2709@gmail.com>
2020-03-29 12:25:36 -0700

    adding icons

--- components/Seo.jsx
+++ components/Seo.jsx
@@ -3,9 +3,9 @@ import Head from "next/head";
 
 export default () => (
     <Head>
-<link rel="icon" type="image/x-icon" href="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.vicksburgnews.com%2Fwp-content%2Fuploads%2F2020%2F02%2FCoronavirus-2019-nCoV-CDC-23312_without_background.png&f=1&nofb=1"/>
+<link rel="icon" type="image/x-icon" href="../public/icon.png"/>
 
-<meta property="image" content="https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/2019-nCoV-CDC-23312_without_background.png/1280px-2019-nCoV-CDC-23312_without_background.png"/>
+<meta property="image" content="../public/icon.png"/>
 
 <meta name="corona virus statistics" content="Corona Virus Live Statistics In The World" />
 
@@ -13,7 +13,7 @@ export default () => (
       rel="icon"
       type="image/png"
       sizes="192x192"
-      href="https://upload.wikimedia.org/wikipedia/commons/thumb/7/72/2019-nCoV-CDC-23312_without_background.png/1280px-2019-nCoV-CDC-23312_without_background.png"
+      href="../public/icon.png"
     />
 
 <meta name="description" content="Corona virus statistics" />
@@ -21,6 +21,5 @@ export default () => (
 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
 <title>Corona Virus</title>
-
     </Head>
 )
